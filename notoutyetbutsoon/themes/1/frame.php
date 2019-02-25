c0c9254a734eba1d514e373db226a366beb6b427
BuildTools <unconfigured@null.spigotmc.org>
2019-02-25 16:28:55 +0100

    init

---
+++ notoutyetbutsoon/themes/1/frame.php
@@ -0,0 +1,11 @@
+ <div class="holds-the-iframe">
+<iframe src="http://webpaint.me/notoutyetbutsoon/themes/1/examples/dashboard.html" style="width:100%;height:120vh;border: 0;"></iframe>
+</div>
+<style>
+.holds-the-iframe {
+  background:url(http://webpaint.me/notoutyetbutsoon/assets/images/load.gif) no-repeat;
+    z-index: 99999;
+    background-size: 100px;
+    background-position: top;
+ }
+</style>
